9a26ad8d208b7355fc5ec8fae61b1d02a7ef1721
ManojKumarDevarakonda <manojdevarakonda786@gmail.com>
2025-02-23 22:37:00 +0530

    loginpage cretaed

---
+++ src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { H1 } from '../components/TextTags'
+
+const Login = () => {
+  return (
+    <>
+      <H1 val="Login" />
+    </>
+  )
+}
+
+export default Login
