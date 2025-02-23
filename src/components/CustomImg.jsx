e4618f362fbe25fd1f745eef76050aeae4f49f8c
ManojKumarDevarakonda <manojdevarakonda786@gmail.com>
2025-02-23 20:58:14 +0530

    initial setup

---
+++ src/components/CustomImg.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CustomImg = (props) => {
+  return (
+    <>
+    <img src={props.src} alt={props.alt} width={props.width} height={props.height} className={props.cls} style={props.style}/>
+    </>
+  )
+}
+
+export default CustomImg
---
+++ src/components/TextTags.jsx
@@ -0,0 +1,40 @@
+export const H1 = (props) =>{
+    return(
+        <h1 style={props.style} className={props.cls}>{props.val}</h1>
+    )
+} 
+export const H2 = (props) =>{
+    return(
+        <h2 style={props.style} className={props.cls}>{props.val}</h2>
+    )
+} 
+export const H3 = (props) =>{
+    return(
+        <h3 style={props.style} className={props.cls}>{props.val}</h3>
+    )
+} 
+export const H4 = (props) =>{
+    return(
+        <h4 style={props.style} className={props.cls}>{props.val}</h4>
+    )
+} 
+export const H5 = (props) =>{
+    return(
+        <h5 style={props.style} className={props.cls}>{props.val}</h5>
+    )
+} 
+export const H6 = (props) =>{
+    return(
+        <h6 style={props.style} className={props.cls}>{props.val}</h6>
+    )
+}
+export const P = (props) =>{
+    return(
+        <p style={props.style} className={props.cls}>{props.val}</p>
+    )
+}
+export const Span = (props) =>{
+    return(
+        <span style={props.style} className={props.cls}>{props.val}</span>
+    )
+} 
